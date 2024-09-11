a6a35c6cbcd66cd92df9e0ba79222728a9c11f51
Ekansh0801 <ekanshaggarwal11103@gmail.com>
2024-09-11 23:14:46 +0530

    Built navbar

--- client/src/MainLayout.tsx
+++ client/src/MainLayout.tsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "./components/Navbar"
 
 
 const MainLayout = () => {
   return (
-    <div>MainLayout</div>
+    <div>
+      <Navbar/>
+      {/* <Outlet/> */}
+
+    </div>
   )
 }
 
